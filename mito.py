7a81dde33be071e2ac6fa0214f6ae1c77a0d6182
robucsc <robucsc@gmail.com>
2021-05-31 20:23:49 -0700

    updated file nomenclature

---
+++ mito.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# Name: Rob Lodes (rlodes), Michael S... (ms...)
+# bme160 final project
+
+import glob
+from mitoUtils import SamReader
+
+
+########################################################################
+# Main
+########################################################################
+
+# def main(inCL=None):
+def main(inFile=None):
+    '''
+    The Main function.
+    Input: FastA sequences from STDIN
+    Output: a list of essential substrings graphically positioned in relation to their position in the sequence
+    Example use: python findUnique.py < dataFile.fa > output.txt
+    '''
+
+    # Returns a list of names in list files.
+    print("Using glob.glob()")
+    files = glob.glob('/**/*.txt', recursive=True)
+    for file in files:
+        print(file)
+
+    for file in files
+        mySam = SamReader(file)  # use this for debugging.
+        # mySam = SamReader() # use this one for the command line (also to turn in)
+
+        for head, seq, in myFasta.readFasta():
+            seq = seq.replace('.', '').replace('_', '').replace('-', '')    # clean up seq
+            tRNA(head, seq)                                                 # instanciate a tRNA
+        tRNA.tRNAlist.sort(key = lambda element:element.head)               # sort base on the header
+        for current in tRNA.tRNAlist:                                       # traverse the tRNAlist
+            # send unique messages to all objects
+            current.findUniques()                                           # call findUniques
+            # send essential messages to all objects
+            current.findEssentials()                                        # call findEssentials
+            current.findPosition()                                          # call findPosition
+            print(current.head.replace(' ', ''))                            # clean up header, and print
+            print(current.seq)                                              # print the seq
+            for element in current.outputList:                              # travers the outputList
+                print(element)                                              # print the element
+
+if __name__ == "__main__":
+    main()
+    # main(inFile='testData.txt')   # use this for debugging
+
+    # glob, read is file paths to a list, and then use that list to read in the files.
---
+++ mitoUtils.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Name: Rob Lodes (rlodes), Michael S... (ms...)
+# bme160 final project
+
+
+import sys
+
+class SamReader:
+    '''
+    Define objects to read FastA files.
+
+    instantiation:
+    thisReader = FastAreader ('testTiny.fa')
+    usage:
+    for head, seq in thisReader.readFasta():
+        print (head,seq)
+    '''
+
+    def __init__(self, fname=''):
+        '''contructor: saves attribute fname '''
+        self.fname = fname
+
+    def doOpen(self):
+        ''' Handle file opens, allowing STDIN.'''
+        if self.fname is '':
+            return sys.stdin
+        else:
+            return open(self.fname)
+
+    def readSam(self):
+        ''' Read an entire FastA record and return the sequence header/sequence'''
+        header = ''
+        sequence = ''
+
+        with self.doOpen() as fileH:
+
+            header = ''
+            sequence = ''
+
+            # skip to first fasta header
+            line = fileH.readline()
+            while not line.startswith('sam'):
+                line = fileH.readline()
+            header = line[1:].rstrip()
+
+            for line in fileH:
+                if line.startswith('sam'):
+                    yield header, sequence
+                    header = line[1:].rstrip()
+                    sequence = ''
+                else:
+                    sequence += ''.join(line.rstrip().split()).upper()
+
+        yield header, sequence
