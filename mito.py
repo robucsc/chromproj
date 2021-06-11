8a288c5a4889973aea6c61d56c5d627edb62b272
robucsc <robucsc@gmail.com>
2021-06-10 19:02:33 -0700

    doc strings/comments updated doc strings comments, and class name

--- mito.py
+++ mito.py
@@ -4,7 +4,7 @@
 # bme160 final project
 
 import glob
-from mitoUtils import SamReader, mitoDictionary
+from mitoUtils import SamReader, MitoDictionary
 
 ########################################################################
 # CommandLine
@@ -41,7 +41,8 @@ class CommandLine():
         self.parser.add_argument('-p', '--path', action='store', default=None, nargs='?',
                                  help='Location of the data files. Format: ./data/*.txt')
         self.parser.add_argument('-d', '--distance', type=int, choices=(0, 3, 5, 7, 10, 25, 100), default=10,
-                                 action='store', help='number of bases to either side of the POI')
+                                 action='store',
+                                 help='Number of bases on either side of the POI. Options: 0, 3, 5, 7, 10, 25, 100')
         self.parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
         if inOpts is None:
             self.args = self.parser.parse_args()
@@ -66,7 +67,7 @@ def main(inFile=None, options=None):
         thisCommandLine.args.path = './data/*.txt'
 
     files = glob.glob(thisCommandLine.args.path, recursive = True)     # Employs glob to return a list of file names
-    myMito = mitoDictionary()
+    myMito = MitoDictionary()
 
     mySam = SamReader(files[0])                             # input the first file
     for row in mySam.readSam():
--- mitoUtils.py
+++ mitoUtils.py
@@ -2,12 +2,23 @@
 # Name: Rob Lodes (rlodes), Michael Farley (mijfarle)
 # PI: Miten Jain
 # bme160 final project
+'''
 
+mitoUtils.py
+
+This is a group of utilities for processing sam files.
+
+Utilities: SamReader, MitoDictionary
+
+'''
 import sys
 import csv
 
 class SamReader:
     '''
+    reads in  a sam-formatted file containing a tab delimited fields, and returns a series of lists
+    Input: a sam file
+    Output: a series of lists containing the fields from each row of the sam file
     '''
 
     def __init__(self, fname=''):
@@ -22,65 +33,79 @@ class SamReader:
             return open(self.fname)
 
     def readSam(self):
-        ''' Read an entire sam file and return one row at a time till EOF'''
+        ''' Read an entire sam file one row at a time till EOF '''
         with self.doOpen() as samFile:
             reader = csv.reader(samFile, dialect='excel-tab') # reads data into a list
             next(reader)   # using next to skip the file header
             for row in reader:
                 yield row
 
-class mitoDictionary:
-
+class MitoDictionary:
+    '''
+        Analyzes a series of lists identifying points of interest, and extracts a sequence for each
+            point of interest.
+        Input: a series of lists
+        Output: formatted output listing the seq name, and for each seq its POI, start and stop
+            in the header, followed by the seq in the form af a fasta file.
+        Methods: __init__, addPrimeRow, addRow, revComp, differencePosition, findFullSeq, findSeq,
+            outputFasta
+    '''
     def __init__(self):
-        self.mitoDict = {}
-        self.baseDict = {13: 'A', 15: 'C', 17: 'T', 19: 'G', 21: 'N'}
-        self.diffPos = []
+        ''' init, bring in the arguments when called '''
+        self.mitoDict = {}                                              # store base information
+        self.baseDict = {13: 'A', 15: 'C', 17: 'T', 19: 'G', 21: 'N'}   # convert from col to base
+        self.diffPos = []                                               # list of difference positions
 
     def addPrimeRow(self, row):
-        rowList = []
-        rowList.append(row[2])
+        ''' adds a row from the prime file to the dictionary '''
+        rowList = []                                        # a list to hold the current row
+        rowList.append(row[2])                              # add row info to the list
         self.mitoDict[row[1]] = rowList                     # add reference base
         self.addRow(row)
 
     def addRow(self, row):
+        ''' adds a row from the non-prime files to the dictionary '''
         try:
             base = self.baseDict[row.index('1', 13, 21)]    # interest, start, end
-            self.mitoDict[row[1]].append(base)
+            self.mitoDict[row[1]].append(base)              # add base to the dictionary
         except:
             # print('failure is not an option')
             pass
 
     def differencePosition(self):
-        for key in self.mitoDict:
-            if(len(set(self.mitoDict[key]))) != 1:
-                self.diffPos.append(key)
+        ''' finds the points of interest '''
+        for key in self.mitoDict:                           # traverse the dictionary
+            if(len(set(self.mitoDict[key]))) != 1:          # if length is not 1 then this is a POI
+                self.diffPos.append(key)                    # add position as a POI
     
     def findFullSeq(self):
+        ''' assembles the full reference sequence '''
         seq = ''
         for key in self.mitoDict:
             seq = seq + self.mitoDict[key][0]
         return (seq)
 
     def findSeq(self, position, distance, index):
+        ''' assembles the reference seq for the POI for the requested file '''
         seq = ''
-        if (position > distance):
+        if (position > distance):                                   # calculate start
             start = position - distance
         else:
             start = distance - position
-        stop = position + distance
+        stop = position + distance                                  # calculate stop
         try:
             for key in range(start, stop):
-                seq = seq + self.mitoDict[str(key)][index + 1]
+                seq = seq + self.mitoDict[str(key)][index + 1]      # add base to seq
         except:
             # print('failure is not an option')
             pass
-
         return (seq)
         
     def outputFasta(self, file, distance, index):
-        self.differencePosition()
-        fileName = file.split('/')[-1]
-        for diff in self.diffPos:
+        ''' creates the output in the fasta format '''
+        self.differencePosition()                                   # find POI
+        fileName = file.split('/')[-1]                              # get file name for header, strip path
+        for diff in self.diffPos:                                   # assemble header and seq output fasta format
             print('> file ' + fileName + 'position ' + diff +
                   '; chrM-' + str(int(diff) - distance) + '-' + str(int(diff) + distance + 1))
             print(self.findSeq(int(diff), distance, index))
